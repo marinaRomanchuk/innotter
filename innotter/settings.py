1d2a81f032382c8093c5695ef86ebd4486701049
Marina <marina.romanchuk23@gmail.com>
2022-05-26 13:12:54 +0300

    adding cors

--- innotter/settings.py
+++ innotter/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user.apps.UserConfig',
-    'page.apps.PageConfig',
+    'corsheaders',
+    'user',
+    'page',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 ROOT_URLCONF = 'innotter.urls'
 
