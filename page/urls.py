63ede400cd619a3cbc0d49df22649f7579bb175c
Marina <marina.romanchuk23@gmail.com>
2022-06-02 15:32:39 +0300

    removing custom router

--- page/urls.py
+++ page/urls.py
@@ -1,50 +1,15 @@
 from django.urls import re_path
 from rest_framework import routers
-from rest_framework.routers import Route, DynamicRoute, SimpleRouter
 
 from .views import PostViewSet, \
     TagViewSet, \
     PageViewSet, \
     GetAcceptRefuseFollowerViewSet
 
-
-class CustomPagesRouter(SimpleRouter):
-
-    routes = [
-        Route(
-            url=r"^{prefix}/$",
-            mapping={"get": "list"},
-            name="{basename}-list",
-            detail=False,
-            initkwargs={"suffix": "List"}
-        ),
-        Route(
-            url=r"^{prefix}/(?P<pk>\d+)/$",
-            mapping={
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            },
-            name="{basename}-detail",
-            detail=True,
-            initkwargs={"suffix": "Detail"}
-        ),
-        DynamicRoute(
-            url=r"^{prefix}/(?P<pk>\d+)/{url_path}/$",
-            name="{basename}-{url_name}",
-            detail=True,
-            initkwargs={}
-        )
-    ]
-
-
 router = routers.SimpleRouter()
 router.register(r"posts", PostViewSet)
 router.register(r"tags", TagViewSet)
-
-custom_router = CustomPagesRouter()
-custom_router.register(r"pages", PageViewSet)
+router.register(r"pages", PageViewSet)
 
 accept_refuse_followers = GetAcceptRefuseFollowerViewSet.as_view(
     {
@@ -58,4 +23,3 @@ urlpatterns = [
     re_path(r"^pages/(?P<pk>\d+)/followers/$", accept_refuse_followers),
 ]
 urlpatterns += router.urls
-urlpatterns += custom_router.urls
